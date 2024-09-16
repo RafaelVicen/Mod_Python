48ee81b0c15b48a0ce61dea081f48fb577496353
RafaelVicen <vicenterafael451@gmail.com>
2024-09-16 13:16:26 +0100

    second

---
+++ 00-aula.py
@@ -0,0 +1,22 @@
+velocidade_internet = 10
+print(velocidade_internet)
+#Quais sao os tipos de dados que nos podemos armazenar na memoria do computador?
+#podemos armazenar:
+#1-Numeros
+velocidade_internet = 10; #eh um tipo de numeros.
+#2-Valores boleanos
+esta_aberta = True; #os valores boleanos em python recebem True ou False (condiçao verdadeira ou falsa) usar o (T & F) Maiusculo
+#3-Strings
+slogan = 'feito eh melhor que perfeito'#aqui nos temos o tipo string (cadeia de caracteres) pode se usar aspas simples
+slogan = "feito eh melhor que perfeito"# deve ser usado quando quero trabalhar com texto (pode se usar aspas duplas)
+#Tudo isso que podemos armazenar, chamamos de tipos primitivos.
+a,b,c,d = 1,2,3,4;
+print(a)
+print(b)
+print(c)
+print(d)
+#como eu posso saber, qual eh o tipo da minha variavel?
+#para saber qual eh o tipo da variavel no python usamos o comando (type)
+print(type(a))
+print(type(velocidade_internet))
+#na saida, vamos ter o resultado do tipo (int) que eh de inteiro
