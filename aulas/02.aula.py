f163e510b631842d6094bf10da00a16df9c14a39
RafaelVicen <vicenterafael451@gmail.com>
2024-09-17 13:05:37 +0100

    "actualizacao"

---
+++ aulas/02.aula.py
@@ -0,0 +1,13 @@
+#Entendendo os conceitos de string
+
+print('Estamos codando todos os dias') # string com aspas simples
+print("Estamos codando todos os dias")# string com aspas duplas
+print('''Estamos codando todos os das
+e estou aprendendo muito ''') #string com aspas triplas, usamos ela para escrever
+
+#caracteres de escape
+
+print("Ola meu nome eh \n Rafael Vicente")# O caracter de escape ou ( barra invertida \n), serve para mudar de linha
+#no exemplo de baixo a variavel nome recebeu (carol)
+nome = 'carol'
+print (len(nome))# Aqui contamos a funcao len, ele conta quantos caracteres tem o nome de cima, len serve pra contar o numero de caracteres.
